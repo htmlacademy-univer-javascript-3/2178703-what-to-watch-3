303440abfc2de46953ec75b8bc64d88bd2e387c3
elena <101496751+Defectl@users.noreply.github.com>
2023-10-18 11:23:04 +0500

    Обратная сторона реальности

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,10 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
-import { ReactNode } from 'react';
 
 type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
-  children: ReactNode;
+  children: JSX.Element;
 }
 
 export default function PrivateRoute(props: PrivateRouteProps) {
